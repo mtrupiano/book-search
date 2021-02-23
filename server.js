abae755eefff0919e04ad35e982332c74a3e5e06
mtrupiano <markt6749@gmail.com>
2021-02-23 11:43:32 -0800

    attempting build fix

--- server.js
+++ server.js
@@ -53,8 +53,17 @@ app.delete('/api/books/:id', (request, response) => {
 
 // Send every other request to the React app
 // Define any API routes before this runs
+let protected = ['transformed.js', 'main.css', 'favicon.ico']
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  let path = req.params['0'].substring(1)
+
+  if (protected.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/build/index.html`);
+  }
 });
 
 
