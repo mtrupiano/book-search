788c0d4e82a67143c988be787014a72015d43424
mtrupiano <markt6749@gmail.com>
2021-02-24 14:36:41 -0800

    push prebuilt to heroku

---
+++ client/build/precache-manifest.873e5db44dad94849ecde9023c00cb20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cded6d944bd41dc8596c0ac5ef00585f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "934b353d32e209e69c44",
+    "url": "/static/css/2.4210231d.chunk.css"
+  },
+  {
+    "revision": "61a4ffdad03cafdbfb40",
+    "url": "/static/css/main.33777e1f.chunk.css"
+  },
+  {
+    "revision": "934b353d32e209e69c44",
+    "url": "/static/js/2.640c5d54.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.640c5d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a4ffdad03cafdbfb40",
+    "url": "/static/js/main.3990644b.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
